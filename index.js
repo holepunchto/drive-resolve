632b53435df35994cb577c327f1633d2d8473412
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-28 14:58:22 +0100

    Ensure `basedir` is treated as a URL directory

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
+const path = require('path')
+const url = require('url')
 const resolve = require('bare-module-resolve')
 
 module.exports = async (drive, id, opts = {}) => {
   const extensions = opts.extensions || ['.js', '.cjs', '.json', '.mjs']
-  const basedir = opts.basedir || '\\'
+  const basedir = opts.basedir || path.sep
 
   const readPackage = async ({ pathname }) => {
     if (await drive.entry(pathname)) {
@@ -12,7 +14,9 @@ module.exports = async (drive, id, opts = {}) => {
     }
   }
 
-  for await (const { pathname } of resolve(id, new URL('file://' + basedir), { extensions }, readPackage)) {
+  const parentURL = url.pathToFileURL(basedir[basedir.length - 1] === path.sep ? basedir : basedir + '/')
+
+  for await (const { pathname } of resolve(id, parentURL, { extensions }, readPackage)) {
     if (await drive.entry(pathname)) {
       return pathname
     }
