5dd6bd6100177bdffb9c80068e8092f668305a98
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-28 20:05:51 +0100

    Safer URL conversion

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = async (drive, id, opts = {}) => {
   const extensions = opts.extensions || ['.js', '.cjs', '.json', '.mjs']
   const basedir = opts.basedir || path.sep
 
-  const readPackage = async ({ pathname }) => {
+  const readPackage = async (packageURL) => {
+    const pathname = url.fileURLToPath(packageURL)
+
     if (await drive.entry(pathname)) {
       const file = await drive.get(pathname)
       return JSON.parse(file.toString())
@@ -16,7 +18,9 @@ module.exports = async (drive, id, opts = {}) => {
 
   const parentURL = url.pathToFileURL(basedir[basedir.length - 1] === path.sep ? basedir : basedir + '/')
 
-  for await (const { pathname } of resolve(id, parentURL, { extensions }, readPackage)) {
+  for await (const moduleURL of resolve(id, parentURL, { extensions }, readPackage)) {
+    const pathname = url.fileURLToPath(moduleURL)
+
     if (await drive.entry(pathname)) {
       return pathname
     }
