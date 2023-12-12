e4c95b395be4bae153389980fe4b776bf2180ca9
Mathias Buus <mathiasbuus@gmail.com>
2023-12-12 17:33:37 +0100

    keys are drive keys, not file paths

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 'use strict'
-const path = require('path')
-const url = require('url')
 const resolve = require('bare-module-resolve')
 const b4a = require('b4a')
+const { URL } = require('url')
 
 module.exports = async (drive, id, opts = {}) => {
   const extensions = opts.extensions || ['.js', '.cjs', '.json', '.mjs']
-  const basedir = opts.basedir || path.sep
+  const basedir = opts.basedir || '/'
   const conditions = opts.runtimes
   const sourceOverwrites = opts.sourceOverwrites || {}
 
   const readPackage = async (packageURL) => {
-    const pathname = url.fileURLToPath(packageURL)
+    const key = fromFileURL(packageURL)
 
-    if (Object.hasOwn(sourceOverwrites, pathname)) {
-      const overwrite = sourceOverwrites[pathname]
+    if (Object.hasOwn(sourceOverwrites, key)) {
+      const overwrite = sourceOverwrites[key]
       return JSON.parse(b4a.toString(overwrite))
     }
 
-    const entry = await drive.entry(pathname)
+    const entry = await drive.entry(key)
     if (entry) {
       const file = await drive.get(entry)
       return JSON.parse(b4a.toString(file))
@@ -27,17 +26,25 @@ module.exports = async (drive, id, opts = {}) => {
     return null
   }
 
-  const parentURL = url.pathToFileURL(basedir[basedir.length - 1] === path.sep ? basedir : basedir + path.sep)
+  const parentURL = toFileURL(basedir[basedir.length - 1] === '/' ? basedir : basedir + '/')
 
   for await (const moduleURL of resolve(id, parentURL, { extensions, conditions }, readPackage)) {
-    const pathname = url.fileURLToPath(moduleURL)
+    const key = fromFileURL(moduleURL)
 
-    if (await drive.entry(pathname)) {
-      return pathname
+    if (await drive.entry(key)) {
+      return key
     }
   }
 
   const err = new Error(`Cannot find module '${id}'`)
   err.code = 'MODULE_NOT_FOUND'
   throw err
+}
+
+function toFileURL (path) {
+  return new URL('file://' + encodeURI(path))
+}
+
+function fromFileURL (url) {
+  return decodeURI(url.pathname)
 }
